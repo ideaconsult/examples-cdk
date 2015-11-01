3434f711d4aebe09eddb4963f9e5fa3bc6e4f8de
vedina <jeliazkova.nina@gmail.com>
2015-11-01 08:53:57 +0200

    Benchmark project using JMH

---
+++ cdk-jmh/src/main/java/net/idea/benchmark/CDKBenchmark.java
@@ -0,0 +1,13 @@
+package net.idea.benchmark;
+
+import org.openjdk.jmh.annotations.Benchmark;
+
+public class CDKBenchmark {
+
+    @Benchmark
+    public void testMethod() {
+        // This is a demo/sample template for building your JMH benchmarks. Edit as needed.
+        // Put your benchmark code here.
+    }
+
+}
