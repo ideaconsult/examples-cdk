564c3d0f34e94741c9e856e09b66ca811605a2d6
vedina <jeliazkova.nina@gmail.com>
2015-11-01 09:08:01 +0200

    adding cdk dependencies

--- cdk-jmh/src/main/java/net/idea/benchmark/CDKBenchmark.java
+++ cdk-jmh/src/main/java/net/idea/benchmark/CDKBenchmark.java
@@ -1,13 +1,22 @@
 package net.idea.benchmark;
 
 import org.openjdk.jmh.annotations.Benchmark;
+import org.openscience.cdk.config.AtomTypeFactory;
+import org.openscience.cdk.exception.NoSuchAtomTypeException;
+import org.openscience.cdk.interfaces.IAtomType;
+import org.openscience.cdk.silent.SilentChemObjectBuilder;
 
 public class CDKBenchmark {
-
+	static AtomTypeFactory factory = AtomTypeFactory.getInstance("org/openscience/cdk/dict/data/cdk-atom-types.owl", SilentChemObjectBuilder.getInstance());
+	
     @Benchmark
-    public void testMethod() {
-        // This is a demo/sample template for building your JMH benchmarks. Edit as needed.
-        // Put your benchmark code here.
+    public void testGetAtomType() throws NoSuchAtomTypeException {
+
+    	IAtomType[] atomtypes = factory.getAllAtomTypes();
+    	for (IAtomType atomtype: atomtypes) {
+    		IAtomType type = factory.getAtomType(atomtype.getAtomTypeName());
+    	}
+
     }
 
 }
