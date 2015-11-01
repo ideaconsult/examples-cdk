f9022a44e38d22575a2241a350c24aa71329395c
vedina <jeliazkova.nina@gmail.com>
2015-11-01 11:30:03 +0200

    using state jmh vars

--- cdk-jmh/benchmark.bat
+++ cdk-jmh/benchmark.bat
@@ -1,14 +1,17 @@
-echo cdk-1.5.12-SNAPSHOT
-call mvn clean install -P cdk-1.5.12-SNAPSHOT
+@echo ON
+@echo cdk-1.5.12-SNAPSHOT
+call mvn clean install package -P cdk-1.5.12-SNAPSHOT
 @echo copy
-call copy target\benchmark*.jar /Y
+copy target\benchmark*.jar . /Y
 
-echo "cdk-1.5.11"
+@echo "cdk-1.5.11"
 call mvn clean install package -P cdk-1.5.11
-echo copy
-call copy target\benchmark*.jar /Y
+@echo copy
+copy target\benchmark*.jar . /Y
 
-echo "run cdk-1.5.11 benchmark"
-call java -jar benchmarks-1.5.11.jar > benchmarks-1.5.11.txt
-echo "run cdk-1.5.12-SNAPSHOT benchmark"
-call java -jar benchmarks-1.5.12-SNAPSHOT.jar > benchmarks-1.5.12-SNAPSHOT.txt
+
+@echo "run cdk-1.5.11 benchmark"
+java -jar benchmarks-1.5.11.jar > benchmarks-1.5.11.txt
+
+@echo "run cdk-1.5.12-SNAPSHOT benchmark"
+java -jar benchmarks-1.5.12-SNAPSHOT.jar > benchmarks-1.5.12-SNAPSHOT.txt
--- cdk-jmh/src/main/java/net/idea/benchmark/CDKBenchmark.java
+++ cdk-jmh/src/main/java/net/idea/benchmark/CDKBenchmark.java
@@ -4,8 +4,16 @@ import java.util.concurrent.TimeUnit;
 
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Measurement;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.TearDown;
+import org.openjdk.jmh.annotations.Warmup;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
@@ -17,38 +25,50 @@ import org.openscience.cdk.silent.SilentChemObjectBuilder;
 
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
+@Warmup(iterations = 5)
+@Measurement(iterations = 5)
+@Fork(1)
+@State(Scope.Benchmark)
 public class CDKBenchmark {
-	final static AtomTypeFactory factory=AtomTypeFactory.getInstance(
-				"org/openscience/cdk/dict/data/cdk-atom-types.owl",
-				SilentChemObjectBuilder.getInstance());
-	
+
+	@State(Scope.Benchmark)
+	public static class BenchmarkFactory {
+		AtomTypeFactory instance;
+
+		@Setup(Level.Trial)
+		public void initialize() {
+			instance = AtomTypeFactory.getInstance(
+					"org/openscience/cdk/dict/data/cdk-atom-types.owl",
+					SilentChemObjectBuilder.getInstance());
+		}
+
+		@TearDown(Level.Trial)
+		public void shutdown() {
+			// Nothing to do
+		}
+	}
 
 	@Benchmark
 	@OutputTimeUnit(TimeUnit.NANOSECONDS)
-	public int testGetAtomType() throws NoSuchAtomTypeException {
+	public int testGetAtomType(BenchmarkFactory factory) throws NoSuchAtomTypeException {
 		int count = 0;
-		IAtomType[] atomtypes = factory.getAllAtomTypes();
+		IAtomType[] atomtypes = factory.instance.getAllAtomTypes();
 		for (IAtomType atomtype : atomtypes) {
-			IAtomType type = factory.getAtomType(atomtype.getAtomTypeName());
+			IAtomType type = factory.instance.getAtomType(atomtype.getAtomTypeName());
 			count++;
 		}
 		return count;
 	}
 
-	@Benchmark
-	@OutputTimeUnit(TimeUnit.NANOSECONDS)
-	public int testGetAtomTypeLoop() throws NoSuchAtomTypeException {
-		int count = 0;
-		IAtomType[] atomtypes = factory.getAllAtomTypes();
-		for (int i = 0; i < 1000; i++)
-			for (IAtomType atomtype : atomtypes) {
-				IAtomType type = factory
-						.getAtomType(atomtype.getAtomTypeName());
-				count++;
-			}
-		return count;
-	}
-
+	/*
+	 * @Benchmark
+	 * 
+	 * @OutputTimeUnit(TimeUnit.NANOSECONDS) public int testGetAtomTypeLoop()
+	 * throws NoSuchAtomTypeException { int count = 0; IAtomType[] atomtypes =
+	 * factory.getAllAtomTypes(); for (int i = 0; i < 1000; i++) for (IAtomType
+	 * atomtype : atomtypes) { IAtomType type = factory
+	 * .getAtomType(atomtype.getAtomTypeName()); count++; } return count; }
+	 */
 	public static void main(String[] args) throws RunnerException {
 		Options opt = new OptionsBuilder()
 				.include(".*" + CDKBenchmark.class.getSimpleName() + ".*")
