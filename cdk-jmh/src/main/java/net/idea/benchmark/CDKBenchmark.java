5bd995a945a8e75296a0f1d2f533253ac4730301
ngn <jeliazkova.nina@gmail.com>
2015-11-02 08:47:54 +0200

    test structure replaced with one with 581 atoms

--- cdk-jmh/src/main/java/net/idea/benchmark/CDKBenchmark.java
+++ cdk-jmh/src/main/java/net/idea/benchmark/CDKBenchmark.java
@@ -1,5 +1,6 @@
 package net.idea.benchmark;
 
+import java.io.InputStream;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.Assert;
@@ -21,17 +22,17 @@ import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 import org.openscience.cdk.atomtype.CDKAtomTypeMatcher;
 import org.openscience.cdk.config.AtomTypeFactory;
-import org.openscience.cdk.config.Isotopes;
 import org.openscience.cdk.exception.CDKException;
 import org.openscience.cdk.exception.NoSuchAtomTypeException;
 import org.openscience.cdk.interfaces.IAtom;
 import org.openscience.cdk.interfaces.IAtomContainer;
 import org.openscience.cdk.interfaces.IAtomType;
-import org.openscience.cdk.interfaces.IBond;
-import org.openscience.cdk.silent.Atom;
+import org.openscience.cdk.io.MDLReader;
 import org.openscience.cdk.silent.AtomContainer;
 import org.openscience.cdk.silent.SilentChemObjectBuilder;
 import org.openscience.cdk.tools.manipulator.AtomTypeManipulator;
+
+import com.google.common.base.Verify;
 
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
@@ -57,67 +58,39 @@ public class CDKBenchmark {
 
 		}
 	}
-	
-	
+
 	@State(Scope.Benchmark)
 	public static class Molecule {
-		IAtomContainer instance;
+		IAtomContainer instance = null;
 
 		@Setup(Level.Trial)
 		public void initialize() {
-
-			IAtomContainer mol = new AtomContainer();
-			mol.addAtom(new Atom("C")); // 0
-			mol.addAtom(new Atom("C")); // 1
-			mol.addAtom(new Atom("C")); // 2
-			mol.addAtom(new Atom("C")); // 3
-			mol.addAtom(new Atom("C")); // 4
-			mol.addAtom(new Atom("C")); // 5
-			mol.addAtom(new Atom("C")); // 6
-			mol.addAtom(new Atom("C")); // 7
-			mol.addAtom(new Atom("C")); // 8
-			mol.addAtom(new Atom("C")); // 9
-			mol.addAtom(new Atom("C")); // 10
-			mol.addAtom(new Atom("C")); // 11
-			mol.addAtom(new Atom("C")); // 12
-			mol.addAtom(new Atom("C")); // 13
-			mol.addAtom(new Atom("C")); // 14
-			mol.addAtom(new Atom("C")); // 15
-			mol.addAtom(new Atom("C")); // 16
-			mol.addAtom(new Atom("C")); // 17
-			mol.addAtom(new Atom("C")); // 18
-
-			mol.addBond(0, 1, IBond.Order.SINGLE); // 1
-			mol.addBond(1, 2, IBond.Order.DOUBLE); // 2
-			mol.addBond(2, 3, IBond.Order.SINGLE); // 3
-			mol.addBond(3, 4, IBond.Order.DOUBLE); // 4
-			mol.addBond(4, 5, IBond.Order.SINGLE); // 5
-			mol.addBond(5, 6, IBond.Order.DOUBLE); // 6
-			mol.addBond(6, 7, IBond.Order.SINGLE); // 8
-			mol.addBond(7, 8, IBond.Order.DOUBLE); // 9
-			mol.addBond(8, 9, IBond.Order.SINGLE); // 10
-			mol.addBond(9, 0, IBond.Order.DOUBLE); // 11
-			mol.addBond(9, 4, IBond.Order.SINGLE); // 12
-			mol.addBond(8, 10, IBond.Order.SINGLE); // 12
-			mol.addBond(10, 11, IBond.Order.DOUBLE); // 12
-			mol.addBond(11, 12, IBond.Order.SINGLE); // 12
-			mol.addBond(12, 13, IBond.Order.DOUBLE); // 12
-			mol.addBond(13, 7, IBond.Order.SINGLE); // 12
-			mol.addBond(3, 14, IBond.Order.SINGLE); // 12
-			mol.addBond(14, 15, IBond.Order.SINGLE); // 12
-			mol.addBond(12, 16, IBond.Order.SINGLE); // 12
-			mol.addBond(16, 17, IBond.Order.SINGLE); // 12
-			mol.addBond(17, 18, IBond.Order.SINGLE); // 12
+			MDLReader reader = null;
 			try {
-				Isotopes.getInstance().configureAtoms(mol);
-			} catch (Exception x) {
+				IAtomContainer mol = null;
+				InputStream in = getClass().getClassLoader()
+						.getResourceAsStream(
+								"net/idea/benchmark/CHEMBL2373570.sdf");
+				Verify.verifyNotNull(in);
+				reader = new MDLReader(in);
+				mol = reader.read(new AtomContainer());
+				instance = mol;
+				Verify.verifyNotNull(instance);
+				Verify.verify(instance.getAtomCount() == 581);
+			} catch (CDKException x) {
 				x.printStackTrace();
-			}
-			instance = mol;
-		}
 
+			} finally {
+				try {
+					if (reader != null)
+						reader.close();
+				} catch (Exception x) {
+				}
+			}
+
+		}
 	}
-	
+
 	@Benchmark
 	@OutputTimeUnit(TimeUnit.NANOSECONDS)
 	public int testGetAtomType(BenchmarkFactory factory)
@@ -130,19 +103,22 @@ public class CDKBenchmark {
 			Assert.assertNotNull(type);
 			count++;
 		}
-		Assert.assertTrue(count>0);
+		Assert.assertTrue(count > 0);
 		return count;
 	}
-	
 
 	@Benchmark
 	@OutputTimeUnit(TimeUnit.NANOSECONDS)
-	public IAtomContainer testPerceiveAtomType(Molecule molecule) throws CDKException {
-	    CDKAtomTypeMatcher matcher = CDKAtomTypeMatcher.getInstance(molecule.instance.getBuilder());
-        for (IAtom atom : molecule.instance.atoms()) {
-            IAtomType matched = matcher.findMatchingAtomType(molecule.instance, atom);
-            if (matched != null) AtomTypeManipulator.configure(atom, matched);
-        }
+	public IAtomContainer testPerceiveAtomType(Molecule molecule)
+			throws CDKException {
+		CDKAtomTypeMatcher matcher = CDKAtomTypeMatcher
+				.getInstance(SilentChemObjectBuilder.getInstance());
+		for (IAtom atom : molecule.instance.atoms()) {
+			IAtomType matched = matcher.findMatchingAtomType(molecule.instance,
+					atom);
+			if (matched != null)
+				AtomTypeManipulator.configure(atom, matched);
+		}
 		return molecule.instance;
 	}
 
